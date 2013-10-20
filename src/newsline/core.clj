05f766e4629b018236c7d50cb4a0a1a0fab2fa83
Aditya Raisinghani <aditya.ajeet@gmail.com>
2013-10-20 11:19:10 +0530

    Managed to tokenize the words and find their POS tags. Need to simplify the words using porters algorithm.

--- src/newsline/core.clj
+++ src/newsline/core.clj
@@ -1,7 +1,55 @@
 (ns newsline.core
-  (:gen-class))
+  (:gen-class)
+  (:use [clj-wordnet.core]
+  					[opennlp.nlp]
+  					[opennlp.tools.filters]
+  					[opennlp.treebank]))
+
+(def get-sentences (make-sentence-detector "models/en-sent.bin"))
+(def tokenize (make-tokenizer "models/en-token.bin"))
+(def pos-tag (make-pos-tagger "models/en-pos-maxent.bin"))
+(def name-find (make-name-finder "models/en-ner-person.bin"))
+(def chunker (make-treebank-chunker "models/en-chunker.bin"))
+
+(def wordnet (make-dictionary "models/dict/"))
+(def s (slurp "blah.txt"))
+(def sentences (get-sentences s))
+
+(defn lemma
+	[word]
+	(:lemma word))
+
+(defn word-net
+	"Returns a word in the IWord format of JWI"
+	[word tag]
+	(first (wordnet word tag)))	
+
+(defn relatedSynsets
+	"Returns the related synsets of the given word"
+	[word]
+	(if (not (= word nil)) (flatten (vals (related-synsets word :similar-to)))))
+
+(defn postagger
+	"Returns whether the word is a noun or a verb"
+	[tag]
+	(if (or (= (.toString tag) "NN") (= (.toString tag) "NNS") (= (.toString tag) "NNP") (= (.toString tag) "NNPS"))
+					"noun" "verb"))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+	"Main driver function."
   [& args]
-  (println "Hello, World!"))
+  		(def tokens (nouns-and-verbs (pos-tag (tokenize s))))
+   		(def counts (count tokens))
+  		(loop [i 0]
+  			(when (< i counts)
+  				(def word ((first tokens) 0))
+  				(def postag (postagger ((first tokens) 1)))
+  				(println (first tokens))
+  				(println postag)
+  				(def wordnetWord (word-net word postag))
+  				(if (not (= wordnetWord nil)) (println (:lemma wordnetWord)))
+  				(def synsetWords (relatedSynsets wordnetWord))
+  				(if (not (= synsetWords nil)) (println (map :lemma synsetWords)))
+  				(def tokens (rest tokens))
+  				(recur (inc i)))))
+
