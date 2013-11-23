70b617121af8715b2986712e8d9062d842a3c8c2
Aditya Raisinghani <aditya.ajeet@gmail.com>
2013-11-23 22:45:16 +0530

    Implemented wordBasket. Added synsets to wordBasket. First round filtering successful.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
   							 [clojure-opennlp "0.3.1"]
   							 [clj-wordnet "0.0.5"]
                  [stemmers "0.2.1"]]
